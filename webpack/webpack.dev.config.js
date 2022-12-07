8a8873705ebc2f26eb2c61e3e09888f3838822ad
fffffkkk <alexuppereks@gmail.com>
2022-12-07 18:37:18 +0300

    adding husky and settings hook for git

--- webpack/webpack.dev.config.js
+++ webpack/webpack.dev.config.js
@@ -1,17 +1,15 @@
-// @ts-nocheck
-
 import path from 'path';
 import common from './webpack.common';
 import { merge } from 'webpack-merge';
 
 module.exports = merge(common, {
 	mode: 'development',
-	devtool: "inline-source-map",
+	devtool: 'inline-source-map',
 	devServer: {
-		static: path.join(__dirname, "build"),
+		static: path.join(__dirname, 'build'),
 		historyApiFallback: true,
 		port: 4000,
 		open: true,
-		hot: true
+		hot: true,
 	},
 });
