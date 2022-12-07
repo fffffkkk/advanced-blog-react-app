d65b03b7ea540d46e8f582768cb3a9544a7a14f6
fffffkkk <alexuppereks@gmail.com>
2022-12-07 22:17:04 +0300

    fix jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  verbose: true,
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
 };
