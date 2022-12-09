184bdb40ca5aafbf0e00465d6e61e11d55677467
fffffkkk <alexuppereks@gmail.com>
2022-12-09 15:57:23 +0300

    addign css loader for dev and prod webpack

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
+import '@/assets/global.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
