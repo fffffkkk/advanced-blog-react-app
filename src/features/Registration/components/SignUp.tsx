6d123100cca3fb6cf0e5d2f617e118fb2b9b6f65
fffffkkk <alexuppereks@gmail.com>
2022-12-09 17:11:46 +0300

    primary registration with redux/toolkit

--- src/features/Registration/components/SignUp.tsx
+++ src/features/Registration/components/SignUp.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 
+import { useActions } from '@/hooks/use-actions';
 import AuthForm from '@/features/Registration/components/form/AuthForm';
+import { useTypedSelector } from '@/hooks/use-typed-selector';
 
 const SignUp = () => {
+	const { addUser } = useActions();
+	const { user } = useTypedSelector((state) => state.user);
+
 	const handleSignUp = (form: { email: string, password: string }) => {
-		console.log('yes', form);
+		addUser({
+			id: Date.now(),
+			name: form.email,
+			token: form.password,
+		});
 	};
+
+	console.log(user);
 
 	return <AuthForm title='Регистрация' submit={handleSignUp} />;
 };
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
+import { store } from '@/store';
 import App from './App';
 import '@/assets/global.css';
 
@@ -11,8 +13,10 @@ if (!rootElement) throw new Error('Failed to find the root element');
 const root = ReactDOM.createRoot(rootElement);
 root.render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<Provider store={store}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</Provider>
 	</React.StrictMode>
 );
